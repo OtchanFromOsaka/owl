05355bb9bc57bca7ee1da7f2e129d412db7dfd70
OtchanFromOsaka <147917062+OtchanFromOsaka@users.noreply.github.com>
2025-03-09 15:06:29 +0900

    Refactor code for consistency and readability, including formatting adjustments and import statement updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import honox from 'honox/vite'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import build from '@hono/vite-build/cloudflare-workers'
+import { defineConfig } from "vite";
+import honox from "honox/vite";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import build from "@hono/vite-build/cloudflare-workers";
 
 export default defineConfig({
-  plugins: [
-    honox({
-      devServer: { adapter },
-      client: { input: ['./app/style.css'] }
-    }),
-    build(),
-  ],
+	plugins: [
+		honox({
+			devServer: { adapter },
+			client: { input: ["./app/style.css"] },
+		}),
+		build(),
+	],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./app", import.meta.url)),
@@ -24,5 +24,4 @@ export default defineConfig({
 			usePolling: true,
 		},
 	},
-})
-
+});
